4272c37a8019a89e21428d855182b1255c473669
Pradeep Yadav <69448799+webpradeep@users.noreply.github.com>
2023-04-20 13:09:24 +0530

    herobanner changes

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -3,7 +3,7 @@ import "./HeroBanner.scss"
 import { useNavigate } from 'react-router-dom'
 import useFetch from '../../../hooks/useFetch'
 import { useSelector } from 'react-redux'
-import Img from '../../../components/lazyloadimage/Img';
+import Img from '../../../components/lazyloadImage/Img';
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper"
 const HeroBanner = () => {
 
