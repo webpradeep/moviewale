8ae024cba2a4b2dbbcb4ccee1ddfd3fce128dccc
Pradeep Yadav <69448799+webpradeep@users.noreply.github.com>
2023-04-20 13:20:58 +0530

    cast changes

--- src/pages/details/detailBanner/DetailBanner.jsx
+++ src/pages/details/detailBanner/DetailBanner.jsx
@@ -6,7 +6,7 @@ import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import useFetch from "../../../hooks/useFetch";
 import Genres from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circleRating/CircleRating";
-import Img from "../../../components/lazyLoadImage/Img.jsx";
+import Img from "../../../components/lazyLoadImage/Img";
 import PosterFallback from "../../../assets/no-poster.png";
 
 import VideoPopup from "../../../components/videoPopup/VideoPopup";
