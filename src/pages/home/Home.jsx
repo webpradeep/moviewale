8fc6ef3ffdf7b6441d86b28df7fecd11583e4efd
Pradeep Yadav <69448799+webpradeep@users.noreply.github.com>
2023-04-20 13:14:58 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Home.scss"
-import HeroBanner from './heroBanner/HeroBanner.jsx'
+import HeroBanner from './heroBanner/HeroBanner'
 import Trending from './trending/Trending'
 import Popular from './popular/Popular'
 import TopRated from './topRated/TopRated'
